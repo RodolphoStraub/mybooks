d43f74fb8666290f1db336272e680a4089847f8b
RodolphoStraub <89853868+RodolphoStraub@users.noreply.github.com>
2024-09-16 20:41:58 -0300

    start

---
+++ src/app/components/Estante.tsx
@@ -0,0 +1,11 @@
+import styles from "./Estante.module.css"
+
+export default function Estante(){
+    return(
+        <>
+        <div className={styles.all}>
+            oi
+        </div>
+        </>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from "./components/Navbar";
+import Estante from "./components/Estante";
+
+export default function Home() {
+  return <>
+  <Navbar/>
+  <Estante/>
+  </>;
+}
